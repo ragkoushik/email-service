ebfe6ba90cbcebb3a0ffed027a02786c05cfaa78
Raghu Koushik <raghu.koushik@iress.com>
2021-01-16 13:09:13 +1100

    Add postman collection for testing

--- templates/passAll.js
+++ templates/passAll.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 
 module.exports = {
     from: process.env.EMAIL_FROM,
-    to: 'ragkoushikp42@gmail.com',
+    to: "ragkoushikp42@gmail.com",
+    cc: "ragkoushik@gmail.com",
     subject: "Pass template",
     type: "text/plain",
     body: "This is a sample template that works in both services",
